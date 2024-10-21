ecc80a712aaf5bcdcfe2377acf38ada08039ed9e
Ponraj2398 <ponrajrahul23@gmail.com>
2024-10-21 17:05:46 +0530

    sweet alert changes

--- src/IndexPage.jsx
+++ src/IndexPage.jsx
@@ -149,7 +149,7 @@ const IndexComponent = () => {
 
                 <div className="sidebar-menu">
                     {/* <a href="/" ><i className="bi bi-box-arrow-right fs-6"></i>Logout</a> */}
-                    <button onClick={{Logout}} style={{border:'none',background:'none'}}><i className="bi bi-box-arrow-right fs-6"></i>Logout</button>
+                    <a onClick={{Logout}} href="/" style={{border:'none',background:'none'}}><i className="bi bi-box-arrow-right fs-6"></i>Logout</a>
                     {/* <button onClick={GoToLogin}><i className="bi bi-box-arrow-right fs-6"></i>
                         Logout</button> */}
                 </div>
